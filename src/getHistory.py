1fe6711f0bb556690ce19334de8bf5480cfd7fbf
Arseny Gubarkov <arseny@gubarkov.pro>
2024-05-09 20:35:49 +0300

    delete

--- src/getHistory.py
+++
@@ -1,6 +0,0 @@
-from typing import AsyncGenerator
-from pyrogram.types import Message
-
-async def get_history(client, start_id: int, end_id: int, chat_id):
-    await client.statrt()
-    messages: AsyncGenerator[Message, None] = client.get_chat_history()
